10949daca608c05dc45ddd626655ce76f581564c
itsneedtoknow <letta_in_love@mail.ru>
2026-07-01 22:40:57 +0300

    feat: add hero block

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,10 +1,14 @@
 let navBurger = document.querySelector(".nav__burger");
 let navMenu = document.querySelector(".nav__menu");
 let dropDownItems = document.querySelectorAll(".nav__item--dropdown");
-function toggleMobileNav() {
+const body = document.body;
+function toggleMobileNav(e) {
+  e.stopPropagation();
+  body.classList.toggle("no-scroll");
   navMenu.classList.toggle("nav__menu--open");
 }
 function openDropDown(e) {
+  e.stopPropagation();
   let currentDropDownItem = e.target.closest(".nav__item--dropdown");
   dropDownItems.forEach((item) => {
     if (item !== currentDropDownItem) {
@@ -17,4 +21,16 @@ function openDropDown(e) {
 navBurger.addEventListener("click", toggleMobileNav);
 dropDownItems.forEach((item) => {
   item.addEventListener("click", openDropDown);
+});
+document.addEventListener("click", (e) => {
+  dropDownItems.forEach((item) => {
+    item.classList.remove("nav__item--dropdown--open");
+  });
+  if (
+    navMenu.classList.contains("nav__menu--open") &&
+    !navMenu.contains(e.target)
+  ) {
+    navMenu.classList.remove("nav__menu--open");
+    body.classList.remove("no-scroll");
+  }
 });
