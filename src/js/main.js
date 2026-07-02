11f1ecb076a570f44d02269054b0c988d9d2ad42
itsneedtoknow <letta_in_love@mail.ru>
2026-07-02 16:20:51 +0300

    feat: add reviews block

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import "../scss/main.scss";
 import "./navigation";
 import "./schedule";
+import "./reviews";
---
+++ src/js/reviews.js
@@ -0,0 +1,60 @@
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation],
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+});
+const reviewCards = document.querySelectorAll(".review");
+
+reviewCards.forEach((item) => {
+  const itemReadMore = item.querySelector(".review__more");
+
+  if (itemReadMore) {
+    itemReadMore.addEventListener("click", function (e) {
+      e.stopPropagation();
+
+      reviewCards.forEach((card) => {
+        if (card !== item) {
+          card.classList.remove("review--expanded");
+          const btn = card.querySelector(".review__more");
+          if (btn) btn.textContent = "Далее";
+        }
+      });
+
+      const isExpanded = item.classList.toggle("review--expanded");
+
+      itemReadMore.textContent = isExpanded ? "Свернуть" : "Далее";
+      swiper.update();
+    });
+  }
+});
+
+// reviewCards.forEach((item) => {
+document.body.addEventListener("click", function (e) {
+  if (!e.target.closest(".review")) {
+    reviewCards.forEach((card) => {
+      card.classList.remove("review--expanded");
+      const btn = card.querySelector(".review__more");
+      if (btn) btn.textContent = "Подробнее";
+      swiper.update();
+    });
+  }
+});
+// });
