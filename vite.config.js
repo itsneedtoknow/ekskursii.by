f52d23a0de72586e9a91ddd8d89d7b09ed7675c1
itsneedtoknow <letta_in_love@mail.ru>
2026-07-02 19:52:15 +0300

    fix: change vite base path to relative

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Задаем базовый путь, равный названию вашего репозитория на GitHub
-  base: "/ekskursii.by/",
+  base: "./",
 });
